b40b663fe0c715a086f0df268886a90537a65fb8
Nikita <nikita.korolev.14.08.2005@mail.ru>
2025-01-29 17:43:15 +0500

    json convertor

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks;
+using JsonConvertor.models;
+
+class Program{
+    static async Task Main(string[] args){
+        HttpClient client = new HttpClient();
+        await GetJson(client);
+    }
+
+    static async Task  GetJson(HttpClient client){
+        var jsontext = await client.GetFromJsonAsync<List<Product>>("http://localhost:5084/getProducts");
+        foreach (var a in jsontext){
+            Console.WriteLine("id = " +a.id + " name = " + a.name);
+            Console.Out.Flush();
+        }
+    }
+}
